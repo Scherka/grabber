68e3f2f635574c77ae0ae6ded7bed5829d2d0b9e
Scherka <s.cher.a@mail.ru>
2024-07-24 17:35:46 +0400

    декомпозировал программу, исправил флаги

--- grabber.go
+++ grabber.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
@@ -16,47 +17,51 @@ func main() {
 	var src, dst string
 	src, dst, err := flagParsing()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("%v \r\n", err)
 		return
 	}
 	err = checkOrCreateDir(dst)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("%v \r\n", err)
 		return
 	}
-	err = readLinesFromFileRunParseMakeHTML(src, dst)
+	urls, err := readLinesFromFile(src)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("%v \r\n", err)
 		return
 	}
+	parseMakeHTML(urls, dst)
 	//время завершения программы
 	finish := time.Since(start).Truncate(10 * time.Millisecond).String()
 	fmt.Println("Время выполнения программы:", finish)
 }
 
-// FlagParsing - обработка флагов
+// flagParsing - обработка флагов
 func flagParsing() (string, string, error) {
 
 	//флаг файла
-	src := flag.String("src", "", "Используйте флаг -stc для введения файла с URL.")
+	src := flag.String("src", "", "используйте флаг -stc для введения файла с URL.")
 	//флаг папки
-	dst := flag.String("dst", "", "Используйте флаг -dst для введения каталога для сохраниня html.")
+
+	dst := flag.String("dst", "", "используйте флаг -dst для введения каталога для сохраниня html.")
 	flag.Parse()
+	//проверка наличия флагов
 	if len(*src) == 0 {
-		return *src, *dst, fmt.Errorf("ошибка: используйте флаг -src для введения файла с URL")
+		flag.PrintDefaults()
+		return "", "", fmt.Errorf("отстутствуют необходимые флаги: -src")
 	}
 	if len(*dst) == 0 {
-		return *src, *dst, fmt.Errorf("ошибка: используйте флаг -dst для введения папки, куда будут сохраняться HTML")
+		flag.PrintDefaults()
+		return "", "", fmt.Errorf("отстутствуют необходимые флаги: -dst")
 	}
 	return *src, *dst, nil
 }
 
-// CheckDir - проверка существования директории и её создание в случае отсутствия
+// checkOrCreateDir - проверка существования директории и её создание в случае отсутствия
 func checkOrCreateDir(path string) error {
 	//проверка существования каталога
 	_, err := os.Stat(path)
 	if err != nil && !os.IsNotExist(err) {
-		//fmt.Println(err)
 		return fmt.Errorf("ошибка при создании каталога %s: %v", path, err)
 	}
 	if os.IsNotExist(err) {
@@ -66,32 +71,40 @@ func checkOrCreateDir(path string) error {
 	return nil
 }
 
-// readLinesFromFileRunParseMakeHTML - построчное чтение url из файла
-func readLinesFromFileRunParseMakeHTML(src string, dst string) error {
-	//создаём группу ожидания
+// readLinesFromFile - построчное чтение url из файла
+func readLinesFromFile(src string) ([]string, error) {
 	//открываем файл
 	file, err := os.Open(src)
 	if err != nil {
-		return fmt.Errorf("ошибка при открытии файла с url: %v", err)
+		return []string{}, fmt.Errorf("ошибка при открытии файла с url: %v", err)
 	}
 	scanner := bufio.NewScanner(file)
-	//проходим все строки документа
+	//создаём срез url-в
+	var urls []string
 	for scanner.Scan() {
-		scan := formatURL(scanner.Text())
-		func() {
-			resp, err := parse(scan)
-			if err != nil {
-				fmt.Println(err)
-
-			} else {
-				err = createHTML(*resp, dst, scan)
-				if err != nil {
-					fmt.Println(err)
-				}
-			}
-		}()
+		//добавляем в срез текущую строку файла
+		urls = append(urls, formatURL(scanner.Text()))
 	}
-	return nil
+	defer file.Close()
+	return urls, nil
+}
+
+// parseMakeHTML - парсинг по url из среза и создание html-файла в случае успеха
+func parseMakeHTML(urls []string, dst string) {
+	for i := 0; i < len(urls); i++ {
+		//попытка парсинга по текущему элементу среза
+		resp, err := parse(urls[i])
+		if err != nil {
+			fmt.Printf("ошибка при парсинге с url %s: %v \r\n", urls[i], err)
+		} else {
+			//попытка создания html-файла для текущего элемента среза
+			err = createHTML(resp, dst, urls[i])
+			if err != nil {
+				fmt.Printf("ошибка при создании HTML-файла страницы %s: %v\r\n", urls[i], err)
+			}
+		}
+
+	}
 }
 
 // formatURL - проверка наличия "http://" в начале строки
@@ -106,21 +119,25 @@ func formatURL(urlWithoutPrefix string) string {
 	return url
 }
 
-// Parse - get-запрос и возврат ответа-
-func parse(url string) (*http.Response, error) {
+// parse - get-запрос и возврат тела ответа
+func parse(url string) ([]byte, error) {
 
 	//отправка get запроса
 	resp, err := http.Get(url)
 	if err != nil {
-		//fmt.Printf("Не удалось открыть '%s' \r\n", url)
 		return nil, fmt.Errorf("ошибка при открытии url %s: %v", url, err)
 	}
-
-	return resp, err
+	//чтение тела ответа
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return b, fmt.Errorf("ошибка при получении тела запроса %s: %v", url, err)
+	}
+	defer resp.Body.Close()
+	return []byte(b), err
 }
 
 // createHTML - создание HTML на основе полученного ответа
-func createHTML(resp http.Response, dst, url string) error {
+func createHTML(resp []byte, dst, url string) error {
 	nameHTML := fmt.Sprintf("%s%s.html", dst, strings.Replace(url, "/", "|", -1))
 	//создание html-файла
 	file, err := os.Create(nameHTML)
@@ -128,7 +145,7 @@ func createHTML(resp http.Response, dst, url string) error {
 		return fmt.Errorf("ошибка при cоздании файла %s: %v", nameHTML, err)
 	}
 	//запись ответа на запрос в файл
-	resp.Write(file)
+	file.Write([]byte(resp))
 	fmt.Printf("Страница %s успешно сохранена \r\n", url)
 	defer file.Close()
 	return nil
